78c790586c128cf18722b028d0efc16db5e301ad
tausif_bin_mozid <b.tausif15@gmail.com>
2025-03-11 02:56:41 +0600

    Updated

--- Class 5/posts/views.py
+++ Class 5/posts/views.py
@@ -45,6 +45,28 @@ def index(request):
     #     content="This is my third post. I hope you like it.",
     #     published=False,
     # )
+    # #LookUp
+    # posts=Post.objects.filter(title__contains="My") #SELECT * FROM posts_post WHERE title LIKE '%My%'
+    # posts=Post.objects.filter(title__icontains="My") #case-insensitive
+    # print(posts)
+    # posts=Post.objects.filter(title__startswith="My") #SELECT * FROM posts_post WHERE title LIKE 'My%'
+    # print(posts)    
+    # posts=Post.objects.filter(title__endswith="post") #SELECT * FROM posts_post WHERE title LIKE '%post'
+    # print(posts)
+    # posts=Post.objects.filter(title__iendswith="POST") #case-insensitive
+    # posts=Post.objects.filter(id__in=[1,2,3]) #SELECT * FROM posts_post WHERE id IN (1,2,3)
+    # print(posts)
+    # posts=Post.objects.filter(id__gt=1) #SELECT * FROM posts_post WHERE id>1
+    # print(posts)
+    # posts=Post.objects.filter(id__gte=1) #SELECT * FROM posts_post WHERE id>=1
+    # print(posts)
+    # posts=Post.objects.filter(id__lt=3) #SELECT * FROM posts_post WHERE id<3
+    # print(posts)        
+    # posts=Post.objects.filter(id__lte=3) #SELECT * FROM posts_post WHERE id<=3
+    # print(posts)
+    # posts=Post.objects.filter(id__range=[1,3]) #SELECT * FROM posts_post WHERE id BETWEEN 1 AND 3
+    # print(posts)
+
 
     Post.objects.all().exclude(published=False) #SELECT * FROM posts_post WHERE published=1
     Post.objects.all().filter(published=False).delete() #DELETE FROM posts_post WHERE published=0
